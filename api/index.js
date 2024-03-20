af24386d2ad56620a307930d780d5ff6404ac080
ngumi22 <mwaurapngumi@gmail.com>
2024-03-20 14:28:41 +0300

    CC

--- api/index.js
+++ api/index.js
@@ -3,7 +3,10 @@ const cors = require('cors');
 const helmet = require('helmet');
 const csrf = require('csurf');
 const rateLimit = require('express-rate-limit');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
+const { body, validationResult } = require('express-validator');
 
 const app = express();
 
@@ -12,7 +15,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(cookieParser());
 app.use(cors()); // Enable CORS for all routes
-
+const csrf = require('csurf');
 // Trust Proxy
 app.set('trust proxy', 1);
 
@@ -24,12 +27,14 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
+// Use csurf middleware
+app.use(csrf({ cookie: true }));
+
 // Middleware to add CSRF token to response locals
 app.use((req, res, next) => {
-  res.locals.csrfToken = req.csrfToken();
+  res.locals.csrfToken = req.csrfToken(); // Now req.csrfToken() should be defined
   next();
 });
-
 // Routes
 app.get("/", (req, res) => {
   res.send("Welcome");
